a9eda4632327719dd0cb9b2c8172f40b9c4cb2ca
Anis Melliti <ansmeliti@gmail.com>
2020-02-01 14:05:42 +0100

    fix the modal

--- client/src/views/modaledit.js
+++ client/src/views/modaledit.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Modal from 'react-modal';
-import { connect } from 'react-redux'
-import {putContact}  from '../js/actions/actions'
-import "./list.css"
+import { connect } from 'react-redux';
+import { putContact } from '../js/actions/actions';
+import './list.css';
 
 const customStyles = {
   content: {
@@ -17,50 +17,103 @@ const customStyles = {
 
 class EditModal extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       modalIsOpen: false,
-      nom: "",
-      prenom: "",
-      email: "",
-      telephone: "",
-      role: "",
-    }
+      nom: '',
+      prenom: '',
+      email: '',
+      telephone: '',
+      role: ''
+    };
   }
-
-
-  openModal = () => {
-    this.setState({ modalIsOpen: true });
-  }
-
-  closeModal = () => {
-    this.setState({ modalIsOpen: false });
-  }
-
- 
 
   render() {
+    const { isOpen, toggle } = this.props;
+    const {
+      _id,
+      nom,
+      prenom,
+      email,
+      telephone,
+      role,
+      action
+    } = this.props.contact;
     return (
       <div>
-        <button className="btn-edit" onClick={this.openModal}>Edit</button>
         <Modal
-          isOpen={this.state.modalIsOpen}
-          onAfterOpen={this.afterOpenModal}
-          onRequestClose={this.closeModal}
+          isOpen={isOpen}
+          onRequestClose={() => toggle(false)}
           style={customStyles}
-          contentLabel="Example Modal"
+          contentLabel='Example Modal'
         >
-        
-          <input type="text" class="form-control" placeholder="nom" id="nom" name="" onChange={(e) => this.setState({ nom: e.target.value })} />
-          <input type="text" class="form-control" placeholder="prenom" id="prenom" name="" onChange={(e) => this.setState({ prenom: e.target.value })} />
-          <input type="text" class="form-control" placeholder="email" id="email" name="" onChange={(e) => this.setState({ email: e.target.value })} />
-          <input type="text" class="form-control" placeholder="telephone" id="telephone" name="" onChange={(e) => this.setState({ telephone: e.target.value })} />
-          <input type="text" class="form-control" placeholder="role" id="role" name="" onChange={(e) => this.setState({ role: e.target.value })} />
+          <input
+            defaultValue={nom}
+            type='text'
+            class='form-control'
+            placeholder='nom'
+            id='nom'
+            name=''
+            onChange={e => this.setState({ nom: e.target.value })}
+          />
+          <input
+            defaultValue={prenom}
+            type='text'
+            class='form-control'
+            placeholder='prenom'
+            id='prenom'
+            name=''
+            onChange={e => this.setState({ prenom: e.target.value })}
+          />
+          <input
+            defaultValue={email}
+            type='text'
+            class='form-control'
+            placeholder='email'
+            id='email'
+            name=''
+            onChange={e => this.setState({ email: e.target.value })}
+          />
+          <input
+            defaultValue={telephone}
+            type='text'
+            class='form-control'
+            placeholder='telephone'
+            id='telephone'
+            name=''
+            onChange={e => this.setState({ telephone: e.target.value })}
+          />
+          <input
+            defaultValue={role}
+            type='text'
+            class='form-control'
+            placeholder='role'
+            id='role'
+            name=''
+            onChange={e => this.setState({ role: e.target.value })}
+          />
 
-
-          <button className="btn btn-outline-dark" onClick={this.closeModal}>close</button>
-          <button className="btn" onClick={() => this.props.putContact({ id: this.props.index, task: [this.state.nom , this.state.prenom , this.state.email , this.state.telephone , this.state.role]  })}>Update</button>
-
+          <button
+            className='btn btn-outline-dark'
+            onClick={() => toggle(false)}
+          >
+            close
+          </button>
+          <button
+            className='btn'
+            onClick={() =>
+              this.props.putContact({
+                id: _id,
+                nom: this.state.nom,
+                prenom: this.state.prenom,
+                email: this.state.email,
+                telephone: this.state.telephone,
+                role: this.state.role
+              })
+            }
+          >
+            Update
+          </button>
         </Modal>
       </div>
     );
@@ -68,7 +121,7 @@ class EditModal extends React.Component {
 }
 
 const mapStateToProps = state => ({ ...state });
-const mapDispatchToProps = (dispatch) => ({
-  putContact : (payload) => dispatch(putContact(payload))
-})
-export default connect(mapStateToProps, mapDispatchToProps)(EditModal)
+const mapDispatchToProps = dispatch => ({
+  putContact: payload => dispatch(putContact(payload))
+});
+export default connect(mapStateToProps, mapDispatchToProps)(EditModal);
